e3074c97969d1a8cbe79506e67a44fffa0ecd647
ramiiar <33881745+ramiiar@users.noreply.github.com>
2021-05-21 17:46:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = "";
+exports.Default_Prefix = "*";
 /* Your Bot All Embeds Color */
 exports.Color = "";
 /* Your Bot Token (Secret) */
-exports.Token = "";
+exports.Token = "ODQ0NzEwMjE1NTU4ODg5NTAz.YKWXsQ.6NQ6NCAPx-oiRh1aIvGoH94I5rY";
 /* Bot's Owner ID */
-exports.OwnerID = "";
+exports.OwnerID = "501552046646886410";
